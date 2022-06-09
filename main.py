cd869c3ae77e26b2d3bf04acf166d1d6733c0710
Amin Mashari <amin0916.m@gmail.com>
2022-06-09 22:36:03 +0430

    time and datasaving added to the main

---
+++ main.py
@@ -0,0 +1,14 @@
+from data import getdata
+from elastic import savedata
+from time import check_time
+
+
+def main():
+    data = getdata()
+    savedata(data)
+
+
+if __name__ == "__main__":
+    if check_time():
+        main()
+        print("done!")
