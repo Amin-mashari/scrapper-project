12ec421d45cb552a245510bd6d63f2162225af83
Amin Mashari <amin0916.m@gmail.com>
2022-06-12 23:34:06 +0430

    adding schedule for project

---
+++ schedule.py
@@ -0,0 +1,16 @@
+from croniter import croniter
+from datetime import datetime
+from config import read_conf_data
+
+base = datetime.now()
+iter = croniter('* * * * *', base)
+
+
+def check_time():
+    print("time logging....")
+
+    while True:
+        now = datetime.now()
+
+        if croniter.match((read_conf_data()["schedule"]["sch_format"]), now):
+            return True
