57714a1b744850b9d9ef2f0341965955cc1c4fb5
Amin Mashari <amin0916.m@gmail.com>
2022-06-13 15:09:53 +0430

    refactor for better use

--- elastic.py
+++ elastic.py
@@ -1,33 +1,47 @@
+from distutils.command.config import config
 from time import sleep
 import traceback
 import elasticsearch
 import json
 
-es = elasticsearch.Elasticsearch("http://elasticsearch:9200")
+CONF = config.read_conf_data()
+
+
+def load_elastic(conf):
+    LINK = "http://{}:{}".format(CONF["elastic"]
+                                 ["LINK"], CONF["elastic"]["PORT"])
+    return elasticsearch.Elasticsearch(LINK)
 
 
 def check_connection():
+    es = load_elastic()
+
     if es.ping():
         return True
     return False
 
 
+def save_in_elstic(d):
+
+    es = load_elastic()
+    es.index(index="test-index", document=d)
+
 def savedata(data):
     print("aa")
     sleep(8)
 
-    if not es.ping():
-        print("ping:{}".format(es.ping()))
+    if not check_connection():
+        print("ping:{}".format(check_connection()))
         print("connection Error")
         print(traceback.format_exc())
         exit(1)
-  
+
     print("ELASTIC CONNECTED...!")
     js = json.loads(data)
     for d in js["results"]:
         # print(d)
         # print(type(doc))
-        resp = es.index(index="test-index",document=d)
+        save_in_elstic(d)
         # print(resp['result'])
 
     # resp = es.get(index="test-index")
