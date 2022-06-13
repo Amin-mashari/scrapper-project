bb61c60867cce685c862876eaf9e47557abb5acf
Amin Mashari <amin0916.m@gmail.com>
2022-06-13 15:05:01 +0430

    refactor config for unittest

--- config.py
+++ config.py
@@ -3,8 +3,49 @@ import yaml
 # Read YAML file
 
 
-def read_conf_data():
-    with open('config.yml', 'r') as f:
+def read_conf_data(path):
+    with open(path, 'r') as f:
         conf_data = yaml.load(f, Loader=yaml.SafeLoader)
 
     return conf_data
+
+
+def isYml(path):
+    if path.endswith(".yaml") or path.endswith(".yml"):
+        return True
+    else:
+        return False
+
+
+def load_config(config_data):
+
+    # [schedule]
+    if not config_data.get("schedule"):
+        return "schedule not found in config.yml !"
+    # [schedule][sch_format]
+    if not isinstance(config_data["schedule"], dict):
+        return "schedule data format is not dictionary !"
+
+     # [elastic]
+    if not config_data.get("elastic"):
+        return "elastic not found in config.yml !"
+
+    # elastic: {}
+    if not isinstance(config_data["elastic"], dict):
+        return "elastic data format is not dictionary !"
+
+    # elastic[HOST]
+
+    if not isinstance(config_data["elastic"]["HOST"], str):
+        return "elastic data format is not string !"
+    
+    if not config_data["elastic"].get("HOST"):
+        return "elastic host is missing in config.yml .."
+
+    if not config_data["elastic"].get("PORT"):
+        return "elastic port is missing in config.yml .."
+
+    if not str(config_data["elastic"]["PORT"]).isdigit():
+        return "elastic data format is not string !"
+
+    return None
